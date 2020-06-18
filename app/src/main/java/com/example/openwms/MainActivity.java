d9c2504b9de7d640fdf7b656702c6b99e112ffa0
Benedict Sim <mistcideneb@gmail.com>
2020-06-18 15:34:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/openwms/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.openwms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.page_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the toolbar menu
+        getMenuInflater().inflate(R.menu.top_nav, menu);
+
+        // Inflate and initialize the bottom menu
+        ActionMenuView bottomBar = (ActionMenuView)findViewById(R.id.bottom_toolbar);
+        Menu bottomMenu = bottomBar.getMenu();
+        getMenuInflater().inflate(R.menu.btm_nav, bottomMenu);
+        for (int i = 0; i < bottomMenu.size(); i++) {
+            bottomMenu.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return onOptionsItemSelected(item);
+                }
+            });
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dashboard:
+                Intent dashboard = new Intent(this,MainActivity.class);
+                startActivity(dashboard);
+            case R.id.orders:
+                Intent orders = new Intent(this,OrdersActivity.class);
+                startActivity(orders);
+            case R.id.products:
+                Intent products = new Intent(this,ProductsActivity.class);
+                startActivity(products);
+            case R.id.accounting:
+                Intent accounting = new Intent(this,AccountingActivity.class);
+                startActivity(accounting);
+            default:
+                return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/openwms/OrdersActivity.java
@@ -0,0 +1,20 @@
+package com.example.openwms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class OrdersActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+    }
+
+}
