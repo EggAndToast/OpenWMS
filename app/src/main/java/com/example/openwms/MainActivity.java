aeaa5d9bf2f98f7d2c7249e5f6dab2249bf32b87
Benedict Sim <mistcideneb@gmail.com>
2020-06-19 16:45:36 +0800

    Setup of Orders Page

--- app/src/main/java/com/example/openwms/MainActivity.java
+++ app/src/main/java/com/example/openwms/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.openwms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ActionMenuView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,10 +16,31 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.net.UnknownServiceException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private String[] candidateNames;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +48,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.page_toolbar);
         setSupportActionBar(myToolbar);
+/*
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId("1:894665555800:android:027e35c6435fa9f1d8af2d") // Required for Analytics.
+                .setProjectId("openinventory-29c18") // Required for Firebase Installations.
+                .setApiKey("AIzaSyDvTTqCpKaV8C6jgG4H1KW8LUWH4sEMT_A") // Required for Auth.
+                .build();
+        FirebaseApp.initializeApp(this, options, "OpenVMS");
+*/
+
     }
+
 
  //https://stackoverflow.com/questions/32808996/android-add-two-toolbars-in-the-same-activity
 
@@ -55,25 +88,91 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.dashboard:
-                Intent dashboard = new Intent(this,MainActivity.class);
-                startActivity(dashboard);
-                return true;
             case R.id.orders:
                 Intent orders = new Intent(this,OrdersActivity.class);
                 startActivity(orders);
+                finish();
                 return true;
             case R.id.products:
                 Intent products = new Intent(this,ProductsActivity.class);
+                finish();
                 startActivity(products);
                 return true;
             case R.id.accounting:
                 Intent accounting = new Intent(this,AccountingActivity.class);
+                finish();
                 startActivity(accounting);
                 return true;
             default:
                 return true;
         }
     }
-    
+
+
+    public class User {
+
+        public String username;
+        public String email;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+    }
+
+    public void addAdaLovelace(View view) {
+        // [START add_ada_lovelace]
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+        // [END add_ada_lovelace]
+    }
+
+    public void getAllUsers(View view) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // [START get_all_users]
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        // [END get_all_users]
+    }
+
+
+
 }
--- app/src/main/java/com/example/openwms/OrdersActivity.java
+++ app/src/main/java/com/example/openwms/OrdersActivity.java
@@ -7,14 +7,68 @@ import androidx.appcompat.widget.Toolbar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class OrdersActivity extends Activity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+public class OrdersActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.page_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the toolbar menu
+        getMenuInflater().inflate(R.menu.top_nav, menu);
+
+        // Inflate and initialize the bottom menu
+        ActionMenuView bottomBar = (ActionMenuView) findViewById(R.id.bottom_toolbar);
+        Menu bottomMenu = bottomBar.getMenu();
+        getMenuInflater().inflate(R.menu.btm_nav, bottomMenu);
+        for (int i = 0; i < bottomMenu.size(); i++) {
+            bottomMenu.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return onOptionsItemSelected(item);
+                }
+            });
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dashboard:
+                Intent dashboard = new Intent(this,MainActivity.class);
+                startActivity(dashboard);
+                finish();
+                return true;
+            case R.id.products:
+                Intent products = new Intent(this,ProductsActivity.class);
+                finish();
+                startActivity(products);
+                return true;
+            case R.id.accounting:
+                Intent accounting = new Intent(this,AccountingActivity.class);
+                finish();
+                startActivity(accounting);
+                return true;
+            default:
+                return true;
+        }
+    }
 }
